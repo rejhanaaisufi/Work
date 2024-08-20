8c5fe45be85c1a9714bbda9e3f07152caf0b3baa
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-08-20 18:05:24 +0200

    Moduli 22

---
+++ Moduli 22/app.js
@@ -0,0 +1,88 @@
+console.log("Good Job Guys!")
+
+var x = 5
+
+// var 123 = 10; nuk duhet mi emertu variablat me numra
+// var *ds = 45; nuk duhet me nis me special characters
+
+var a = 50 + 50 
+console.log(a)
+
+var b = 10
+var c = 20
+var d = b + c 
+console.log(d)
+
+var g = c - b 
+console.log(g)
+
+var k = 10
+var l = 4;
+var m = k % 1 
+console.log(m)
+
+
+var n = 99 
+n++  // lesaj lloj menyre i thojn postfix 
+console.log(n)
+
+var o = 101;
+o--
+console.log(o)
+
+var p = 5
+var q = 2
+var h = p ** q  //5 ** 2
+var e = 15
+e +=5 // e = 15 +5
+console.log(e) // 20
+e -=5
+console.log(e)  // 15
+
+//////////////////////////////
+var u = 10  // u - 10
+u *=3  // u = 10*3
+console.log(u)  //u =  30
+u /=2  // u = 30 / 2
+console.log(u) // u = 15
+
+/////////////////////////////////////
+var t = 5
+var i =5
+console.log(t == i)  // 5 = 5
+
+var n1 = 6
+var n2 = 7
+console.log(n1 != n2) // true 
+console.log(n1 = n2) // false
+console.log(n1 > n2)  // 6 > 7 ??  false  
+console.log(n1<n2)  //6 < 7 ? true 
+
+var numri = 123
+var tekst = "1232"
+console.log(numri === tekst) // false me tre = e kqyr edhe te dhenat me 2 = vetem llojin e te dhenave
+
+var firstName ="Digjital"
+var lastName = "School"
+var fullName = firstName +           " " + lastName
+console.log(fullName)            
+
+var school = "School"
+console.log(`Hello from ${school}`)
+
+// AND Operator - duhen mu plotesu krejt kushtet qe me dal shprehja  TRUE
+var age = 24 
+console.log(age >= 18 && age <=65)
+
+// OR POperator - Vetem nese nje kusht eshte True, shprehja na kthen TRUE
+var num1 = 5
+var num2 = 6
+console.log(num1==5 || num2 == 5)
+
+var num3 = 9
+var num4 = 10
+console.log(!(num3==num4));
+
+
+
+
