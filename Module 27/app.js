4dddc19456e2b1b23a78ccc5465e9f0af7f57004
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-08-29 16:20:28 +0200

    ..

---
+++ Module 27/app.js
@@ -0,0 +1,28 @@
+// Do while loop
+
+var i =0;
+
+do{
+//    i++
+   i=i+1
+   console.log(i)
+}while(i < 5)
+
+
+    console.log("_________________")
+    // while loop
+
+    var j =0
+    while(j<5){
+        j++
+        console.log(j)
+    }
+ 
+    console.log("_________________")
+
+//   for loop 
+for(var i = 0; i<=6; i++){
+    console.log("Value of i is"+i)
+}
+
+
