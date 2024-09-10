d7923b2f666bf6c89fd72b04e6b2903ed6a94c56
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-09-10 17:02:34 +0200

    ..

---
+++ Module 33(on-class)/js/main.js
@@ -0,0 +1,15 @@
+$('#sort').sortable();
+
+$(function(){
+    $("#accordion").accordion({
+        collapsible:true
+    });
+});
+
+$( function(){
+    $("#tabs").tabs({
+        collapsible:true
+    });
+});
+
+
