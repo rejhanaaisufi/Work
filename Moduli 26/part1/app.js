19f6f88e4b839255fd67d983f7374c8096b56805
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-09-11 17:17:23 +0200

    lkiho

---
+++ Moduli 26/part1/app.js
@@ -0,0 +1,37 @@
+var input = document.getElementById("input_id")
+var button = document.getElementById("btn_id")
+var text = document.getElementById("text_id")
+
+var input1 = document.getElementById("input1_id")
+var input2 = document.getElementById("input2_id")
+var btn2 = document.getElementById("btn2_id")
+var ans = document.getElementById("answer")
+
+//var input1 = document.querySelector("#input1_id")
+
+
+btn2.addEventListener("click", function(){
+    ans.innerHTML = Int(input1.value)  + Int(input2.value)
+})
+
+button.onclick = function(){
+    if(input.value > 10){
+        text.innerHTML = "Input Value is greater than 10"
+     }
+     else if(input.value < 10){
+        text.innerHTML = "Input value is less than 10"
+     }
+     else{
+        text.innerHTML = "Input value is equal to 10"
+     }
+
+
+
+
+
+
+
+
+
+
+}
