1d79b8a43a8832764b571f01dd54b1c9d2486a7c
rejhanaaisufi <167358364+rejhanaaisufi@users.noreply.github.com>
2024-10-08 19:10:18 +0200

    /..

---
+++ Module 38/index.php
@@ -0,0 +1,27 @@
+<?php
+
+ $username = $_GET['username'];
+ $password = $_GET['password'];
+ echo $username;
+ echo "<br>";
+ echo $password;
+ 
+//  include_once 'config.php';
+//  include_once ('config.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <form action="add.php" method="POST">
+            <input type="text" name="name" placeholder="Name"></br>
+            <input type="text" name="surname" placeholder="Surname"></br>
+            <input type="email" name="email" placeholder="Email"></br>
+            <button type="submit" name="submit">Add</button>
+        </form>
+
+    </body>
+</html> 
